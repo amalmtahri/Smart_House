89fd8850344ba82a281b2d27ad33e2fd0369ab09
ext_mtahri <mtahriamal0@gmail.com>
2022-03-28 15:04:29 +0000

    navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HouseComponent } from './components/house/house.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HouseComponent,
+    NavbarComponent,
     
   ],
   imports: [
