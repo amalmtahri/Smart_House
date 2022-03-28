fb579283ebfcbd5d58e9d71f36bd65699b43dd32
ext_mtahri <mtahriamal0@gmail.com>
2022-03-28 14:56:01 +0000

    smartHome component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HouseComponent } from './components/house/house.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    HouseComponent,
     
   ],
   imports: [
